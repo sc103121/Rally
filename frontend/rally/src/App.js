6399a2de15a6249a969a2ba912caf6e7df4e1eaa
David Yashgur <dy93@cornell.edu>
2024-10-05 12:34:45 -0400

    router

--- frontend/rally/src/App.js
+++ frontend/rally/src/App.js
@@ -2,27 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import background, { Background } from './main/main.jsx';
 import { Bar } from './main/horizontalBar.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Events from './pages/Events';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Background/>
-      <Bar/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event" element={<Events />} />
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   {/* <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+    //   <Background/>
+    //   <Bar/>
+    // </div>
   );
 }
 
---
+++ frontend/rally/src/pages/Events.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Events() {
+  return <h2>Events</h2>;
+}
+
+export default Events;
