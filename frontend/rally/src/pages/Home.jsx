0d0099fbbaf3dca3e6318ca8029bb13a4ede39b2
David Yashgur <dy93@cornell.edu>
2024-10-05 12:59:00 -0400

    search

--- frontend/rally/src/pages/Home.jsx
+++ frontend/rally/src/pages/Home.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Search from "../components/search";
 
 function Home() {
-  return <h2>Home Page</h2>;
+  return (<div>
+
+  <h2>Home Page</h2>;
+  <Search />
+  </div>)
 }
 
 export default Home;
