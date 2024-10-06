79bc37f10680ead1a3748434bbd62e4a071446a4
David Yashgur <dy93@cornell.edu>
2024-10-05 20:17:08 -0400

    commit

--- frontend/rally/src/components/event_thumbnail.jsx
+++ frontend/rally/src/components/event_thumbnail.jsx
@@ -2,56 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-// export const events = [
-//   {
-//     id: 1,
-//     title: "Cornell Campus Rally",
-//     description:
-//       "Join us for a peaceful protest to raise awareness about climate change.",
-//     date: "2023-10-15",
-//     time: "12:00 PM",
-//     location: "Cornell University, Ithaca, NY",
-//     goal: 1000,
-//     raised: 300,
-//     isPrivate: false,
-//   },
-//   {
-//     id: 2,
-//     title: "Cornell Sustainability March",
-//     description: "March with us to promote sustainable practices on campus.",
-//     date: "2023-11-05",
-//     time: "2:00 PM",
-//     location: "Cornell University, Ithaca, NY",
-//     goal: 500,
-//     raised: 100,
-//     isPrivate: false,
-//   },
-//   {
-//     id: 3,
-//     title: "Cornell Diversity and Inclusion Rally",
-//     description:
-//       "Stand together to support diversity and inclusion within our community.",
-//     date: "2023-12-01",
-//     time: "1:00 PM",
-//     location: "Cornell University, Ithaca, NY",
-//     goal: 100,
-//     raised: 50,
-//     isPrivate: true,
-//   },
-//   {
-//     id: 4,
-//     title: "Cornell Mental Health Awareness Walk",
-//     description:
-//       "Walk to raise awareness about mental health issues and support services.",
-//     date: "2024-01-20",
-//     time: "11:00 AM",
-//     location: "Cornell University, Ithaca, NY",
-//     goal: 100,
-//     raised: 10,
-//     isPrivate: false,
-//   },
-// ];
-
 export default function Event_thumbnail() {
   const url = "http://localhost:3001/events/get_events";
   const [events, setEvents] = useState([]);
